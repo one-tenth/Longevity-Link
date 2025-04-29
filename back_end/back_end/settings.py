8b033adb097ebe0cb67dd1b60ec559e3d97bd484
one-tenth <11056010@ntub.edu.tw>
2025-04-30 01:03:47 +0800

    看起來不錯其實也還好

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite',
+    'back_end',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
