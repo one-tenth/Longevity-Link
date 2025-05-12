a34cc15d6fd42fcd5229450cac7fde4607981276
one-tenth <11056010@ntub.edu.tw>
2025-05-12 12:58:14 +0800

    db

--- back_end/mysite/admin.py
+++ back_end/mysite/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Family,User,Hos,HealthCare,Med,CallRecord,Scam
 
+
+
+class FamilyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Family._meta.fields]  # 自動顯示所有欄位
+class UserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in User._meta.fields]  # 自動顯示所有欄位
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Hos._meta.fields]  # 自動顯示所有欄位
+class HealthCareAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in HealthCare._meta.fields]  # 自動顯示所有欄位
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Med._meta.fields]  # 自動顯示所有欄位
+class CallRecordAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CallRecord._meta.fields]  # 自動顯示所有欄位
+class ScamAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Scam._meta.fields]  # 自動顯示所有欄位
 # Register your models here.
+admin.site.register(Family,FamilyAdmin)
+admin.site.register(User,UserAdmin)
+admin.site.register(Hos,HospitalAdmin)
+admin.site.register(HealthCare,HealthCareAdmin)
+admin.site.register(Med,MedicineAdmin)
+admin.site.register(CallRecord,CallRecordAdmin)
+admin.site.register(Scam,ScamAdmin)
--- back_end/mysite/models.py
+++ back_end/mysite/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.utils import timezone
 
-# Create your models here.
+class Family(models.Model):
+    FamilyID = models.AutoField(primary_key=True)
+    Fcode = models.CharField(max_length=50)
+    FamilyName = models.CharField(max_length=10)
+    Created_Time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Family"
+        verbose_name_plural = "Family"
+
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=10)
+    Gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
+    Borndate = models.DateField()
+    Phone = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+8869\d{8}$')], unique=True)
+    Password = models.CharField(max_length=20)
+    FamilyID = models.ForeignKey(Family, on_delete=models.CASCADE, db_column='FamilyID')
+    CaregiverID = models.IntegerField()
+    Relationship = models.CharField(max_length=5)
+    Created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "User"
+
+class Hos(models.Model):
+    HosId = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE, db_column='UserID')
+    ClinicPlace = models.CharField(max_length=20)
+    ClinicDate = models.DateField()
+    Doctor = models.CharField(max_length=10)
+    Num = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Hos"
+        verbose_name_plural = "Hos"
+
+class HealthCare(models.Model):
+    HealthID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE, db_column='UserID')
+    BloodP = models.CharField(max_length=10)
+    BloodS = models.CharField(max_length=10)
+    Pluse = models.CharField(max_length=5)
+    Numsteps = models.CharField(max_length=5)
+    Date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Health care"
+        verbose_name_plural = "Health care"
+
+class Med(models.Model):
+    MedId = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE, db_column='UserID')
+    Disease = models.CharField(max_length=10)
+    MedName = models.CharField(max_length=10)
+    MedNote = models.CharField(max_length=50)
+    MedTime = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Med"
+        verbose_name_plural = "Med"
+
+class CallRecord(models.Model):
+    CallId = models.AutoField(primary_key=True)
+    UserId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='UserId')
+    PhoneName = models.CharField(max_length=10)
+    Phone = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+8869\d{8}$')], unique=True)
+    PhoneTime = models.CharField(max_length=10)
+    IsScam = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Call record"
+        verbose_name_plural = "Call record"
+
+class Scam(models.Model):
+    ScamId = models.AutoField(primary_key=True)
+    Phone = models.ForeignKey(CallRecord, on_delete=models.CASCADE, db_column='Phone')
+    Category = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "Scam"
+        verbose_name_plural = "Scam"
