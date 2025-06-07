c831a29986dd2f7d52a0486d345109383abc10ef
Chei <11056016@ntub.edu.tw>
2025-06-07 22:22:45 +0800

    test

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,24 +1,3 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import OcrScreen from './screens/OcrScreen';
-import ResultScreen from './screens/ResultScreen'; // 新增的頁面
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer
-      onStateChange={(state) => {
-        const currentRoute = state?.routes[state.index];
-        console.log('🧭 現在在頁面：', currentRoute?.name);
-      }}
-    >
-      <Stack.Navigator initialRouteName="OcrScreen">
-        <Stack.Screen name="OcrScreen" component={OcrScreen} options={{ title: '拍照辨識' }} />
-        <Stack.Screen name="Result" component={ResultScreen} options={{ title: '辨識結果' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
+import { createStackNavigator } from '@react-navigation/stack';
