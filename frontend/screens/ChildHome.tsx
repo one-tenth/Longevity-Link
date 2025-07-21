9d1ca219fbd607a92c50b7e7ff1baf0a437b2811
Chei <11056016@ntub.edu.tw>
2025-07-22 00:41:19 +0800

    新版介面(圖片改用套件)

--- frontend/screens/ChildHome.tsx
+++ frontend/screens/ChildHome.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../App'; // 確認 App.tsx 裡定義了這個
+import { RootStackParamList } from '../App';
 
-// ChildHome 頁面的 navigation 型別
+// ✅ CLI 專用 icon import 方法
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+
 type ChildHomeNavProp = StackNavigationProp<RootStackParamList, 'ChildHome'>;
 
 export default function ChildHome() {
@@ -12,22 +17,23 @@ export default function ChildHome() {
 
   return (
     <View style={styles.container}>
+      {/* 標題列 */}
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.navigate('Setting')}>
-          <Image source={require('../img/childhome/13866.png')} style={styles.setting} />
+          <Feather name="settings" size={28} color="#fff" />
         </TouchableOpacity>
-        <Text style={styles.title}>CareMate</Text>
-        <Image source={require('../img/childhome/logo.png')} style={styles.logo} />
       </View>
 
+      {/* 使用者資訊 */}
       <View style={styles.userBox}>
-        <Image source={require('../img/childhome/image.png')} style={styles.userIconLarge} />
+        <FontAwesome name="user-circle" size={60} color="#00474f" style={styles.userIconLarge} />
         <Text style={styles.userText}>爺爺</Text>
-        <Image source={require('../img/childhome/61456.png')} style={styles.edit} />
+        <Feather name="edit-2" size={20} color="#030852" />
       </View>
 
-      <View style={styles.alertBox}> 
-        <Image source={require('../img/childhome/2058160.png')} style={styles.alertIcon} />
+      {/* 警示區塊 */}
+      <View style={styles.alertBox}>
+        <MaterialIcons name="warning" size={40} color="#e74c3c" style={styles.alertIcon} />
         <View>
           <Text style={styles.alertText}>時間：20:00</Text>
           <Text style={styles.alertText}>來電號碼：</Text>
@@ -35,201 +41,140 @@ export default function ChildHome() {
         </View>
       </View>
 
+      {/* 功能按鈕 1 */}
       <View style={styles.gridRow}>
-        <TouchableOpacity style={[styles.gridBox, { backgroundColor: '#549D77' }]}> 
-          <Image source={require('../img/childhome/Group.png')} style={styles.locate} />
-          <Text style={styles.gridText1}>即時位置</Text>
+        <TouchableOpacity style={[styles.gridBox, { backgroundColor: '#00474f' }]}>
+          <Entypo name="location-pin" size={40} color="#fff" />
+          <Text style={styles.gridTextWhite}>即時位置</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridBox, { backgroundColor: '#F4C80B' }]}> 
-          <Image source={require('../img/childhome/Vector.png')} style={styles.health} />
-          <Text style={styles.gridText}>健康狀況</Text>
+        <TouchableOpacity style={[styles.gridBox, { backgroundColor: '#030852' }]}>
+          <MaterialIcons name="favorite" size={40} color="#fff" />
+          <Text style={styles.gridTextWhite}>健康狀況</Text>
         </TouchableOpacity>
       </View>
+
+      {/* 功能按鈕 2 */}
       <View style={styles.gridRow}>
-        <TouchableOpacity 
-          style={[styles.gridBox1, { backgroundColor: '#F58402' }]} 
+        <TouchableOpacity
+          style={[styles.gridBox, { backgroundColor: '#006d77' }]}
           onPress={() => navigation.navigate('Medicine')}>
-          <Image source={require('../img/childhome/image-3.png')} style={styles.medcine} />
-          <Text style={styles.gridText}>用藥</Text>
+          <MaterialIcons name="medical-services" size={40} color="#fff" />
+          <Text style={styles.gridTextWhite}>用藥</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={[styles.gridBox1, { backgroundColor: '#65B6E4' }]}
-          onPress={() => navigation.navigate('HospitalRecord')}       >
-          <Image source={require('../img/childhome/4320350.png')} style={styles.hospital} />
-          <Text style={styles.gridText}>看診</Text>
+          style={[styles.gridBox, { backgroundColor: '#03506f' }]}
+          onPress={() => navigation.navigate('HospitalRecord')}>
+          <MaterialIcons name="local-hospital" size={40} color="#fff" />
+          <Text style={styles.gridTextWhite}>看診</Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={[styles.callBox, { backgroundColor: '#F4C80B' }]}> 
-        <Image source={require('../img/childhome/image-1.png')} style={styles.phone} />
+
+      {/* 通話紀錄 */}
+      <TouchableOpacity style={styles.callBox}>
+        <Feather name="phone-call" size={30} color="#fff" />
         <Text style={styles.callText}>通話紀錄</Text>
       </TouchableOpacity>
 
+      {/* 切換帳號 */}
       <TouchableOpacity onPress={() => navigation.navigate('index')}>
-        <Text style={styles.alertText2}>切換使用者</Text>
+        <Text style={styles.switchText}>切換使用者</Text>
       </TouchableOpacity>
-
     </View>
-
-    
   );
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    backgroundColor: '#FCFEED', 
-    alignItems: 'center' 
+  container: {
+    flex: 1,
+    backgroundColor: '#e6f4ff',
+    alignItems: 'center',
   },
   header: {
     width: '100%',
-    height:70,
-    flexDirection: 'row', 
-    justifyContent: 'space-between',
-    backgroundColor: '#65B6E4',
-    position: 'relative',
-    marginBottom:20,
-    paddingLeft:10,
-    paddingRight:10,
-  },
-  logo: { 
-    width: 60, 
-    height: 60,
-    marginTop:15,
-  },
-  setting:{
-    width: 40, 
-    height: 40,
-    marginTop:15,
-  },
-  title: { 
-    fontSize: 50, 
-    fontWeight:'900', 
-    color: '#000', 
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    backgroundColor: '#00474f',
+    paddingHorizontal: 20,
+    alignItems: 'center',
   },
   userBox: {
-    width: '90%', 
-    height:65,
+    width: '90%',
+    backgroundColor: '#fff',
     borderRadius: 10,
+    marginTop: 20,
+    padding: 10,
     flexDirection: 'row',
     alignItems: 'center',
-    borderWidth: 3,
+    elevation: 3,
   },
-  userIconLarge: { 
-    width: 62, 
-    height: 62, 
-    textAlign:'center',
-    marginTop:2,
+  userIconLarge: {
+    marginRight: 20,
   },
-  userText: { 
-    fontSize: 36, 
-    fontWeight: '900', 
-    marginLeft:50,
-    marginTop:5,
-  },
-  edit:{
-    width: 20, 
-    height: 20, 
-    marginLeft:100,
-    marginTop:20,
+  userText: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#030852',
+    flex: 1,
   },
   alertBox: {
     width: '90%',
-    height:100,
-    marginTop:20,
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    borderRadius: 10, 
-    borderWidth: 3, 
-    paddingLeft:5,
-    paddingRight:10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginTop: 20,
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    elevation: 3,
   },
-  alertIcon: { 
-    width: 60, 
-    height: 60, 
-    marginRight:20,
-    marginLeft:5, 
+  alertIcon: {
+    marginRight: 15,
   },
-  alertText: { 
-    fontSize: 20, 
-    fontWeight: '900' 
+  alertText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#030852',
   },
   gridRow: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: '90%',
-    marginTop:10,
+    marginTop: 15,
   },
   gridBox: {
-    width: '45%', 
-    borderRadius: 10, 
-    alignItems: 'center', 
-    borderWidth: 3, 
+    width: '48%',
+    borderRadius: 12,
+    paddingVertical: 16,
+    alignItems: 'center',
+    elevation: 3,
   },
-  gridBox1: {
-    width: '45%', 
-    borderRadius: 10, 
-    alignItems: 'center', 
-    borderWidth: 3, 
-  },
-  gridText: { 
-    fontSize: 20, 
-    fontWeight: '900',  
-    marginTop: 6, 
-    textAlign: 'center',
-  },
-  gridText1: { 
-    fontSize: 20, 
-    fontWeight: '900',  
+  gridTextWhite: {
+    fontSize: 18,
+    fontWeight: 'bold',
     color: '#fff',
-    marginTop: 6, 
-    textAlign: 'center',
-  },
-  locate: {
-    width: 50, 
-    height: 50, 
-    marginTop:5
-  },
-  hospital: {
-    width: 55, 
-    height: 55, 
-    marginTop:5
-  },
-  medcine: {
-    width: 50, 
-    height: 50, 
-    marginTop:5
-  },
-  health: {
-    width: 54, 
-    height: 50,
-    marginTop:5 
-  },
-  phone: {
-    width: 54, 
-    height: 50, 
+    marginTop: 8,
   },
   callBox: {
     width: '90%',
-    height:70,
-    marginTop:20,
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    borderRadius: 10, 
-    borderWidth: 3, 
-    paddingLeft:5,
-    paddingRight:10,
+    marginTop: 20,
+    backgroundColor: '#00474f',
+    borderRadius: 12,
+    paddingVertical: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3,
   },
-  callText: { 
-    fontSize: 20, 
-    fontWeight: '900',
-    marginLeft: 10 
+  callText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginLeft: 10,
   },
-
-  alertText2: {
-    fontSize: 20, 
-    fontWeight: '900',
-    marginLeft:220,
-    marginTop:30
-  }
+  switchText: {
+    fontSize: 16,
+    color: '#030852',
+    fontWeight: 'bold',
+    marginTop: 25,
+  },
 });
-// export default ChildHome;
